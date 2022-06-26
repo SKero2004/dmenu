dec173fc9bbca481af86cf34e25c0f34b2eb76a2
SKero2004 <toyatakahashi522@gmail.com>
2022-06-26 12:31:12 -0800

    updated for new computer

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Terminus:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -15,8 +15,8 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 25;
-static unsigned int min_lineheight = 25;
+static unsigned int lineheight = 28;
+static unsigned int min_lineheight = 28;
 
 /*
  * Characters not considered part of a word while deleting words
